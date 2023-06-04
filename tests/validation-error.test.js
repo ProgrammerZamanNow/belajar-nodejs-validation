4ebbc370d06fd2b5fae201cbd5a143b61cab6962
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-06-04 19:42:10 +0700

    Validation Error

---
+++ tests/validation-error.test.js
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+describe('Joi', function () {
+    it('should return validation error', function () {
+        const usernameSchema = Joi.string().min(5).email().required();
+
+        const result = usernameSchema.validate("ups");
+        console.info(result);
+
+        if(result.error){
+            result.error.details.forEach(detail => {
+                console.info(`${detail.path} = ${detail.message}`);
+            });
+        }
+    });
+});
